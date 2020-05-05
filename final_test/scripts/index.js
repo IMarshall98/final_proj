51d6c808b79fd2caf6741e32af8ea49f5de64914
IMarshall98 <58204512+IMarshall98@users.noreply.github.com>
2020-05-05 16:23:56 -0400

    Updating CSS

---
+++ final_test/scripts/index.js
@@ -0,0 +1,14 @@
+const homePage = document.querySelector('homeBtn');
+homePage.onclick = function() {
+  alert('hello');
+}
+
+const myButton = document.querySelector('button');
+myButton.onclick = function() {
+  alert('hello');
+}
+
+const myButton = document.querySelector('button');
+myButton.onclick = function() {
+  alert('hello');
+}
