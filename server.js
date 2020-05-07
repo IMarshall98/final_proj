04de8c4da07d462d85c2f3ca842d291afd0996f7
Leon <l.miro98@gmail.com>
2020-05-06 23:33:16 -0400

    add points to the map

--- server.js
+++ server.js
@@ -11,49 +11,29 @@ app.use(express.json());
 app.use(express.static('public'));
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json'; // Enter the URL for the data you would like to retrieve here
-
-  // Your Fetch API call starts here
-  // Note that at no point do you "return" anything from this function -
-  // it instead handles returning data to your front end at line 34.
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json'; 
   fetch(baseURL)
     .then((r) => r.json())
     .then((data) => {
-      const clearEmptyData = data.filter((f) => f.geocoded_column_1);
+      const clearEmptyData = data.filter((f) => f.geocoded_column);
       const refined = clearEmptyData.map((m) => ({
-        category: m.category,
-        name: m.name,
-        latLong: m.geocoded_column_1.coordinates
+        organization: m.organization,
+        lat: m.latitude,
+        long: m.longitude
       }));
       return refined;
     })
     .then((data) => {
-      return data.reduce((result, current) => {
-        if (!result[current.category]) {
-          result[current.category] = [];
-        }
-        result[current.category].push(current);
-        return result;
-      }, {});
-    })
-    .then((data) => {
-      const reformattedData = Object.entries(data).map((m) => {
-        console.log(m);
-        return {
-          y: m[1].length,
-          label: m[0]
-        };
-      });
-      return reformattedData;
-    })
-    .then((data) => {
       console.log(data);
       res.send({ data: data });
-      console.log(data); // here's where we return data to the front end
+      console.log(data);
     })
     .catch((err) => {
       console.log(err);
       res.redirect('/error');
     });
 }
+
+app.get('/api', (req, res) => { processDataForFrontEnd(req, res) });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
