096a72fc6af3c2f1a54ff37f90d0be1b5862846d
Leon <l.miro98@gmail.com>
2020-05-06 20:43:10 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,28 +1,59 @@
-// These are our required libraries to make the server work.
-// We're including a server-side version of Fetch to build on your client-side work
 const express = require('express');
 const fetch = require('node-fetch');
 
-// Here we instantiate the server we're going to turn on
 const app = express();
 
-
-// Servers are often subject to the whims of their environment.
-// Here, if our server has a PORT defined in its environment, it will use that.
-// Otherwise, it will default to port 3000
 const port = process.env.PORT || 8080;
 
-// Our server needs certain features - like the ability to send and read JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// And the ability to serve some files publicly, like our HTML.
 app.use(express.static('public'));
-
 
 function processDataForFrontEnd(req, res) {
   const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // Enter the URL for the data you would like to retrieve here
 
+  // Your Fetch API call starts here
+  // Note that at no point do you "return" anything from this function -
+  // it instead handles returning data to your front end at line 34.
+  fetch(baseURL)
+    .then((r) => r.json())
+    .then((data) => {
+      const clearEmptyData = data.filter((f) => f.geocoded_column_1);
+      const refined = clearEmptyData.map((m) => ({
+        category: m.category,
+        name: m.name,
+        latLong: m.geocoded_column_1.coordinates
+      }));
+      return refined;
+    })
+    .then((data) => {
+      return data.reduce((result, current) => {
+        if (!result[current.category]) {
+          result[current.category] = [];
+        }
+        result[current.category].push(current);
+        return result;
+      }, {});
+    })
+    .then((data) => {
+      const reformattedData = Object.entries(data).map((m) => {
+        console.log(m);
+        return {
+          y: m[1].length,
+          label: m[0]
+        };
+      });
+      return reformattedData;
+    })
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data });
+      console.log(data); // here's where we return data to the front end
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/error');
+    });
 }
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
